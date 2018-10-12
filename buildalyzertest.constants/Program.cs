f7937f528986b6c7f8fada09a7ebffe386a17209
itn3000 <itn3000@gmail.com>
2018-10-12 17:43:25 +0900

    first commit

---
+++ buildalyzertest.constants/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Buildalyzer;
+using Buildalyzer.Workspaces;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+namespace buildalyzertest.constants
+{
+    class Program
+    {
+        static void OutputNamespaceMembers(INamespaceOrTypeSymbol ns)
+        {
+            switch(ns)
+            {
+                case ITypeSymbol t:
+                    Console.WriteLine($"ns={ns}, {t.Name}");
+                    break;
+                case INamespaceSymbol nst:
+                    foreach(var child in nst.GetMembers())
+                    {
+                        OutputNamespaceMembers(child);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var analyzer = new AnalyzerManager();
+            var proj1 = analyzer.GetProject("../sandboxproject/sandboxproject.csproj");
+            var ws = proj1.GetWorkspace();
+            var compilation = ws.CurrentSolution.Projects.First().GetCompilationAsync().Result;
+            foreach(var typeSymbol in compilation.Assembly.GlobalNamespace.GetMembers())
+            {
+                OutputNamespaceMembers(typeSymbol);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ sandboxproject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sandboxproject
+{
+    #if DEF1
+    public class X1
+    {
+        // this should be included 
+    }
+    #endif
+    #if DEF2
+    public class X2
+    {
+        // this should not be included 
+    }
+    #endif
+    public class X3
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
